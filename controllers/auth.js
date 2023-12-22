5ceed63f91d51ff7d2da54361036479d11f95bdb
Prabakaran.M <karanpk0108@gmail.com>
2023-12-22 18:45:22 +0530

    Added SendOTP and Register routes

---
+++ controllers/auth.js
@@ -0,0 +1,69 @@
+import nodemailer from "nodemailer";
+import bcrypt from "bcrypt";
+import randomstring from "randomstring";
+import User from "../models/User.js";
+
+// Email and OTP
+export const sendOtp = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const otp = randomstring.generate({
+      length: 6,
+      charset: "numeric",
+    });
+
+    // Save OTP in the database with user's email
+    await User.findOneAndUpdate({ email }, { otp }, { upsert: true });
+
+    // Send the OTP to the user's email
+    // Nodemailer template
+    const transporter = nodemailer.createTransport({
+      service: "gmail.com",
+      auth: {
+        user: process.env.USER,
+        pass: process.env.APP_PASS,
+      },
+    });
+    console.log("Recipient's Email:", email);
+    const mailOptions = {
+      from: "support@globallegals.com",
+      to: email,
+      subject: "OTP for Registration",
+      text: `Your OTP is: ${otp}`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    res.json({ success: true, message: "OTP sent successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Failed to send OTP" });
+  }
+};
+
+// Register Route
+export const register = async (req, res) => {
+  try {
+    const { email, password, otp } = req.body;
+
+    // Validate OTP
+    const user = await User.findOne({ email, otp });
+
+    if (!user) {
+      return res.status(400).json({ success: false, message: "Invalid OTP" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.findOneAndUpdate(
+      { email, otp },
+      { password: hashedPassword, isVerified: true, otp: null },
+      { new: true }
+    );
+
+    res.json({ success: true, message: "Registration successful" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Registration failed" });
+  }
+};
---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import { sendOtp, register } from "./controllers/auth.js";
+import connectDB from "./config/ConnectDB.js";
+
+const app = express();
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Routes
+app.post("/api/sendotp", sendOtp);
+app.post("/api/register", register);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
